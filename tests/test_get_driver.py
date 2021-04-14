a1771ead2d26cb61461a4a0d9faf627169325a87
Andrei Zhidelev <mcjanz@gmail.com>
2021-04-14 21:33:36 +0300

    Python versions (#37)
    * Python versions
    
    * Tests updates

--- tests/test_get_driver.py
+++ tests/test_get_driver.py
@@ -6,18 +6,18 @@ import os
 @pytest.mark.unit
 class TestChrome:
     def test_download_chromedriver(self):
-        get_driver.download_driver("https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_win32.zip")
+        get_driver.download_driver("https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_win32.zip")
         assert os.path.exists("chromedriver_win32.zip") is True
         assert os.path.exists("chromedriver.exe") is True
 
     def test_get_last_release(self):
-        assert get_driver.get_last_release() == "88.0.4324.96"
+        assert get_driver.get_last_release() == "89.0.4389.23"
 
     def test_get_last_release_for_build(self):
         assert get_driver.get_last_release(build="87") == "87.0.4280.88"
 
     def test_compose_driver_download_link(self):
         assert (
-            get_driver.compose_download_link("88.0.4324.96")
-            == "https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_win32.zip"
+            get_driver.compose_download_link("89.0.4389.23")
+            == "https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_win32.zip"
         )
