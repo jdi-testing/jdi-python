4927e38acc5f3d7ffaa1527c1f1854eb8ccdec40
vklonin <vklonin@gmail.com>
2024-03-19 11:40:44 +0300

    removing obsolete scripts

--- utils/get_driver.py
+++
@@ -1,6 +0,0 @@
-from JDI.web.selenium.driver.selenium_driver_factory import SeleniumDriverFactory
-
-if __name__ == "__main__":
-    driver_factory = SeleniumDriverFactory()
-    driver = driver_factory.get_driver()
-    driver.quit()
